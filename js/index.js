4a81ce38e9344a6203c9673c987d001fc47e4b42
JEONMINJU <junielle.jeon@gmail.com>
2021-01-06 23:00:08 +0900

    일단 커밋,,

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 var bannerInterval;
 var bannerIdx = 0;
 
+
 /************이벤트선언************/
 $(".modal-trigger").click(onModalShow);
 $(".modal-container").click(onModalHide);
@@ -14,20 +15,46 @@ $(".header-wrapper").find(".bt-close").click(onTypeHide);
 
 bannerInterval = setInterval(onBannerInterval, 5000);
 
-$(".main-wrapper").mouseover(onMainOver);
+$(".main-wrapper").mouseover(onMainOver); //오버했을때 잠깐 멈춰있게, 버튼 누를때 겹치지 않게,
 $(".main-wrapper").mouseleave(onMainLeave);
 
 $(".main-wrapper .bt-prev").click(onMainPrev);
 $(".main-wrapper .bt-next").click(onMainNext);
 $(".main-wrapper .pager").click(onMainPager);
 
-$(".icon-wrapper .cart").hover(onSubCart); // sub cart !  dropdown
-$(".icon-wrapper .cart").mouseleave(onSubCartLeave); // sub cart !  up
+$(".icon-wrapper .cart-wrap").hover(onSubCart); // sub cart !  dropdown
+$(".icon-wrapper .cart-wrap").mouseleave(onSubCartLeave); // sub cart !  up
+
+$(".navi-wrapper .navi.navi-home").mouseenter(onNaviShow);
+$(".navi-wrapper .navi.navi-home").mouseleave(onNaviHide);
+
+function onNaviShow() {
+	$(".navi-wrapper .navi.navi-home .sub-navi-wrapper").stop().slideDown();
+}
+function onNaviHide() {
+	$(".navi-wrapper .navi.navi-home .sub-navi-wrapper").stop().slideUp();
+}
+
+
+
+$(".sub-wrap ul.sub > .aw-txt").hover(onArrowShow);
+$(".sub-wrap ul.sub > .aw-txt").mouseleave(onArrowHide);
+
+function onArrowShow() {
+	$(".sub-wrap ul.sub > .aw-txt .aw").stop().addClass("active");
+}
+function onArrowHide() {
+	$(".sub-wrap ul.sub > .aw-txt .aw").stop().removeClass("active");
+}
+
+
+
+
 
 /************이벤트콜백************/
 function onMainPrev() {
 	bannerIdx = bannerIdx == 0 ? 3 : bannerIdx - 1 ;
-	mainAni();
+	mainAni(); 
 }
 
 function onMainNext() {
@@ -36,12 +63,12 @@ function onMainNext() {
 }
 
 function onMainPager() {
-	bannerIdx = $(this).index();
+	bannerIdx = $(this).index(); //나의 나(MainPager) 인덱스 번호
 	mainAni();
 }
 
 function onMainOver() {
-	clearInterval(bannerInterval);
+	clearInterval(bannerInterval); //셋인터벌 기능 지우기
 }
 
 function onMainLeave() {
@@ -81,10 +108,10 @@ function onBannerInterval() {
 
 // 카트 드롭다운
 function onSubCart() {
-	 $(".header-wrapper .sub-cart").addClass('active');
+	 $(".cart-wrap ul.cart-dropbox").addClass('active');
 }
 function onSubCartLeave() {
-	$(".header-wrapper .sub-cart").removeClass('active');
+	$(".cart-wrap ul.cart-dropbox").removeClass('active');
 }
 
 function onTypeShow() {
@@ -97,8 +124,8 @@ function onTypeHide() {
 
 /************사용자함수************/
 function mainAni() {
-	$(".main-wrapper .pager").removeClass("active");
-	$(".main-wrapper .pager").eq(bannerIdx).addClass("active");
-	$(".main-wrapper .banner").stop().fadeOut(300);
+	$(".main-wrapper .pager").removeClass("active"); //나머지 애들은 리무브 해달라.
+	$(".main-wrapper .pager").eq(bannerIdx).addClass("active"); //나의 선택된 인덱스번호에 액티브를 주고
+	$(".main-wrapper .banner").stop().fadeOut(300); 
 	$(".main-wrapper .banner").eq(bannerIdx).stop().fadeIn(300);
-}
+} //이게 원래 메인슬라이드 bannerInterval에 있던 기능인데 공통으로 사용자 함수에 mainAni()로 만듬
